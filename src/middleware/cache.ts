d9f8ad2bac9e375aa3e077e8631f6a06181f11a7
Bran Marquez <branloop@protonmail.com>
2023-03-25 22:40:52 -0600

    Cache
    add cache and env example

---
+++ src/middleware/cache.ts
@@ -0,0 +1,14 @@
+import getExpeditiousCache from 'express-expeditious'
+
+const defaultOptions = {
+    namespace: 'expresscache',
+    defaultTtl: '15 minute', //TODO: 60 * 1000
+    statusCodeExpires: {
+        404: '5 minutes',
+        500: 0 // 1 minute in milliseconds
+    }
+}
+
+const cacheInit = getExpeditiousCache(defaultOptions)
+
+export { cacheInit }
